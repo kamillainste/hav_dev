e506ffdc90fd33b7a7050af156960e0719b516b6
Kamilla Instefjord <78148920+kamillainste@users.noreply.github.com>
2026-04-30 13:05:14 +0000

    Oppdatert

--- notater.sh
+++ notater.sh
@@ -11,22 +11,6 @@ nextclade run data/Sekvens* --input-dataset data/3a_AJ299464.3/ --output-all tmp
 # fungerer for IIIa, men ikke for alle genotyper
 
 
-
-# Make the algorithm MORE tolerant of mismatches
-nextclade run data/Sekvens* \
-  --input-dataset data/vp1-2b-junction/ \
-  --penalty-mismatch -1 \
-  --score-match 2 \
-  --output-all tmp
-
-#Try these adjustments (from most to least permissive):
-    #Most permissive: --penalty-mismatch -1 --score-match 2
-    #Moderate: --penalty-mismatch -2 --score-match 3
-    #Stricter: --penalty-mismatch -5 --score-match 5
-
-# ikke testet noe av dette enda
-
-
 nextclade run data/Sekvens* \
   --input-dataset data/vp1-2b-junction/ \
   --alignment-preset high-diversity \
@@ -36,8 +20,24 @@ nextclade run data/Sekvens* \
 #Jobber videre med nextclade_vp1_2b_alginment.sh
 
 
-nextclade_vp1_2b_alignment.sh ./data/
+./nextclade_vp1_2b_alignment.sh ./data/
 # videre arbeid: få result.tsv til å ha heading "strain" istedenfor "seqName", 
   # og fjern index kolonnen slik at strain blir første kolonne
   # dette for å kunne legge den over treet i auspice og bruke til å fargelegge
+  # kan vente med dette til de genotype-spesifikke analysene
+  
+  # ved å velge node-type i auspice får man alle sekvensene man la til i analysen markert
+  # dersom vi ikke har andre sekvenser som også legges til i analysen er jo det dekkende for 
+    ## å se sine prøver, men er jo fint å få med litt mer metadata også
+
+# et problem med å bruke dette data-settet er at referansen er kortere enn mange av sekvensene våre, så det blir mye "missing data" i analysen. 
+  # men hvis vi bare skal bruke det til å bestemme genotype, så gjør ikke det noe
+
+
+
+
+nextclade run data/Sekvens* \
+  --input-dataset data/IIIA_AJ299464.3/ \
+  --alignment-preset high-diversity \
+  --output-all output_IIIA_AJ299464.3_high-diversity/
   
